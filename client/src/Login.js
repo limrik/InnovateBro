d88561cc7560cbd1eb593cf5250f41c3ca5fe27a
rik <vslimrik@gmail.com>
2023-05-29 21:21:38 +0800

    login and register page

---
+++ client/src/Login.js
@@ -0,0 +1,138 @@
+import { Form, Button } from "react-bootstrap";
+import { useNavigate, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Login() {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+      email: "",
+      password: "",
+    });
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+  
+    useEffect(() => {
+      if (Object.keys(formErrors).length === 0 && isSubmit) {
+        handleSubmit();
+      }
+    }, [formErrors]);
+  
+    const handleSubmit = async (e) => {
+      try {
+        await axios
+          .post("/", user)
+          .then((res) => {
+            if (res.data === "pass ok") {
+              navigate("/home");
+            } else if (res.data === "pass not ok") {
+              alert("Wrong password");
+              setUser({
+                email: "",
+                password: "",
+              });
+            } else if (res.data === "does not exist") {
+              alert("Please sign up first Bro");
+              navigate("/register");
+            }
+          })
+          .catch((e) => {
+            alert("wrong details");
+            console.log(e);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  
+    const validate = (values) => {
+      const errors = {};
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+      const passwordRegex =
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+      if (!values.email) {
+        errors.email = "Email is required";
+      } else if (!emailRegex.test(values.email)) {
+        errors.email = "Please enter a valid email";
+      }
+      if (!values.password) {
+        errors.password = "Password is required";
+      } else if (!passwordRegex.test(values.password)) {
+        errors.password = "Please enter a valid password";
+      }
+      return errors;
+    };
+  
+    const handleClick = async (e) => {
+      e.preventDefault();
+      setFormErrors(validate(user));
+      setIsSubmit(true);
+    };
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setUser((prev) => {
+        return {
+          ...prev,
+          [name]: value,
+        };
+      });
+    };
+  
+    return (
+      <div className="flex items-center justify-center min-h screen bg-bg1 bg-no-repeat bg-cover">
+        <div className="relative flex flex-col m-40 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+          <div className="flex flex-col justify-center p-8 md:p-14">
+            <span className="mb-3 text-4xl font-bold font-poppins">
+              Welcome to travelBro!
+            </span>
+            <span className="font-light text-gray-500 mb-8 font-poppins">
+              Start planning your travel itinerary today!
+            </span>
+            <Form>
+              <Form.Group>
+                <Form.Label class="mb-2 text-md font-poppins">Email</Form.Label>
+                <Form.Control
+                  name="email"
+                  value={user.email}
+                  placeholder="Email"
+                  onChange={handleChange}
+                />
+                <p>{formErrors.email}</p>
+                <Form.Label class="mb-2 text-md font-poppins">
+                  Password
+                </Form.Label>
+                <Form.Control
+                  name="password"
+                  type="password"
+                  value={user.password}
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <p>{formErrors.password}</p>
+              <Button
+                onClick={handleClick}
+                variant="primary"
+                className="w-full text-white p-2 rounded-2xl mb-6 font-poppins"
+              >
+                Login
+              </Button>
+              <div id="signInDiv" className="justify-center"></div>
+              <div>
+                <span className="text-center text-gray-400 font-poppins">
+                  {" "}
+                  Don't have an account yet?{" "}
+                </span>
+                <Link to={"/register"} className="text-black font-poppins">
+                  Sign up for free!{" "}
+                </Link>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Login;
--- server.js
+++ server.js
@@ -1,13 +1,94 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const connection_string = process.env.MONGO_URL;
+const port = process.env.PORT || 3001;
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+mongoose
+  .connect(connection_string)
+  .then(() => console.log("mongoDB connection success!"))
+  .catch((err) => console.log(err));
+
+require("./users");
+const collection = mongoose.model("users");
+
+module.exports = collection;
+
 
 app.get("/", (req, res) => {
   res.send("Express is here");
 });
 
-app.listen(3001, function () {
-  console.log("Server is running");
-});
+app.post("/", async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+    };
+  
+    try {
+      const check = await collection.findOne({ email: email });
+  
+      if (check) {
+        const passOk = bcrypt.compareSync(password, check.password);
+        if (passOk) {
+          jwt.sign(
+            { email: check.email, id: check._id },
+            jwtSecret,
+            {},
+            (err, token) => {
+              if (err) throw err;
+              res.cookie("token", token).json("pass ok");
+            }
+          );
+        } else {
+          res.json("pass not ok");
+        }
+      } else {
+        res.json("does not exist");
+      }
+    } catch (e) {
+      res.json("does not exist");
+    }
+  });
+  
+  app.post("/registerUser", async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    const data = {
+      name: name,
+      email: email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    };
+  
+    try {
+      const check = await collection.findOne({ email: email });
+  
+      if (check) {
+        res.json("exist");
+      } else {
+        res.json("does not exist");
+        await collection.insertMany([data]);
+      }
+    } catch (e) {
+      res.json("does not exist");
+    }
+  });
+  
+  app.listen(3001, function () {
+    console.log(`Server is running on port ${port}`);
+  });
